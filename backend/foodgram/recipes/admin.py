76036f6499cd334490a2a76b9374b8d9a42b2603
evgenii_bykovskii <bykovsky.ssau@mail.ru>
2023-06-26 13:41:10 +0400

    added base admin models for recipes

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from .models import Ingredient, Tag, Recipe, ShoppingCart, Favorite
 
-admin.site.register(Ingredient)
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author',)
+    list_filter = ('author', 'name', 'tag',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.site_header = 'Административная страница проекта Foodgram'
 admin.site.register(Tag)
-admin.site.register(Recipe)
 admin.site.register(ShoppingCart)
 admin.site.register(Favorite)
