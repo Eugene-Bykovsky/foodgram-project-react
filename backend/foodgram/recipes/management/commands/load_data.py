56e26ef88451c5a9e3bbd33f940a0a7a401ebef4
evgenii_bykovskii <bykovsky.ssau@mail.ru>
2023-07-13 22:39:19 +0400

    changed load_data command

--- backend/foodgram/recipes/management/commands/load_data.py
+++ backend/foodgram/recipes/management/commands/load_data.py
@@ -7,7 +7,8 @@ from recipes.models import Ingredient
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        with open('recipes/data/ingredients.csv', encoding='utf-8') as file:
+        file_path = options['path']
+        with open(file_path, encoding='utf-8') as file:
             csv_list = csv.reader(file)
             for row in csv_list:
                 name, measurement_unit = row
