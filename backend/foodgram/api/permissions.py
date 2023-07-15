260361d92acbfad76e88de8ed4fd78f3c5b178d5
evgenii_bykovskii <bykovsky.ssau@mail.ru>
2023-07-15 23:10:47 +0400

    fixes pep8 bugs

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -12,5 +12,6 @@ class IsAdminOrAuthorOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated and
-                request.user == obj.author or request.user.is_superuser)
+                or request.user.is_authenticated
+                and request.user == obj.author
+                or request.user.is_superuser)
