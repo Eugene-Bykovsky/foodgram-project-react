576cace9cfc0e89a1fdfcbcb04c9a03a0b3e54a4
evgenii_bykovskii <bykovsky.ssau@mail.ru>
2023-07-17 00:45:35 +0400

    changed vieset recipe

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -91,12 +91,6 @@ class RecipeViewSet(UserViewSet):
             return RecipeSerializer
         return RecipeCreateSerializer
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-    def perform_update(self, serializer):
-        serializer.save(author=self.request.user)
-
     @action(detail=True, methods=['post', 'delete'],
             permission_classes=(permissions.IsAuthenticated,))
     def favorite(self, request, **kwargs):
