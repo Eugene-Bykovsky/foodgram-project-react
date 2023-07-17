62d0c049dea96640527bd3cc399c6a21597f02d5
evgenii_bykovskii <bykovsky.ssau@mail.ru>
2023-07-17 15:55:56 +0400

    changed filter

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,7 +1,7 @@
 from django_filters import AllValuesMultipleFilter, BooleanFilter
 from django_filters.rest_framework import FilterSet
-from rest_framework.filters import SearchFilter, filters
 from recipes.models import Recipe, Tag
+from rest_framework.filters import SearchFilter, filters
 
 
 class IngredientFilter(SearchFilter):
