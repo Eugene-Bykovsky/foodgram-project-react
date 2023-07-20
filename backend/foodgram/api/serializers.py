d48bb5b589f6f64a33b8239bebdbc9344e1bdaa4
evgenii_bykovskii <bykovsky.ssau@mail.ru>
2023-07-20 15:42:27 +0400

    changed serializers

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -202,7 +202,7 @@ class FavoriteSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Favorite
-        fields = 'recipe', 'user'
+        fields = '__all__'
 
     def to_representation(self, instance):
         request = self.context.get('request')
@@ -217,7 +217,7 @@ class ShoppingCartSerializer(FavoriteSerializer):
 
     class Meta:
         model = ShoppingCart
-        fields = 'recipe', 'user'
+        fields = '__all__'
 
     def to_representation(self, instance):
         request = self.context.get('request')
