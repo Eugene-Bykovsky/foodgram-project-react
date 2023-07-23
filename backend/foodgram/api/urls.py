97952a998bbd40fefd7004e006342dcaf36d5adb
evgenii_bykovskii <bykovsky.ssau@mail.ru>
2023-07-23 14:04:03 +0400

    added two new routes to api urls

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import IngredientViewSet, RecipeViewSet, TagViewSet, UsersViewSet
+from .views import (IngredientViewSet, RecipeViewSet, TagViewSet, UsersViewSet,
+                    FavoriteViewSet, ShoppingCartViewSet)
 
 app_name = 'api'
 
@@ -11,7 +12,11 @@ router.register('ingredients', IngredientViewSet, basename='ingredients')
 router.register('tags', TagViewSet, basename='tags')
 router.register('users', UsersViewSet, basename='users')
 router.register('recipes', RecipeViewSet, basename='recipes')
-
+router.register(r'recipes/(?P<recipe_id>\d+)/favorite', FavoriteViewSet,
+                basename='favorite')
+router.register(r'recipes/(?P<recipe_id>\d+)/shopping_cart',
+                ShoppingCartViewSet,
+                basename='shoppingcart')
 
 urlpatterns = (
     path('', include(router.urls)),
